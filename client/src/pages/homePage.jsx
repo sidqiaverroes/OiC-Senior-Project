1dcdb343f9c07fbec4bc36d89e52a0e9c98144ab
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-03-22 21:03:35 +0700

    feat: add initial setup files.

---
+++ client/src/pages/homePage.jsx
@@ -0,0 +1,16 @@
+function homePage() {
+  return (
+    <div className="flex flex-col bg-purple-600 h-full w-full justify-center">
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className=" text-5xl font-extrabold text-yellow-400">
+          Welcome to OiC app
+        </h1>
+        <p className="text-xl font-medium text-white">
+          This app is still under construction.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default homePage;
