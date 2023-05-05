105927596720616d0bdef3488daf7eecfddc93aa
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-05-05 21:36:55 +0700

    modify homepage component by adding more component: navbar, hero, about, feature

---
+++ client/src/components/Features.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import FeatureImg1 from "../assets/Feature-1.png";
+import FeatureImg2 from "../assets/Feature-2.png";
+
+function Features() {
+  return (
+    <div className="flex h-auto bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-8 lg:flex-row py-20">
+        <div className="flex flex-row gap-4 justify-center items-center">
+          <div className=" max-w-lg flex flex-col gap-2 text-purple-900 pr-32">
+            <p className=" text-gray-800 font-bold">Our Features</p>
+            <p className="font-extrabold text-4xl break-words">
+              What Things Do We Provide?
+            </p>
+          </div>
+
+          <div className="flex flex-row gap-8 text-black">
+            <div className=" max-w-xs flex flex-col bg-purple-600 p-8 rounded-3xl justify-center items-center text-white text-center hover:-translate-y-2 transition-transform">
+              <div className=" overflow-hidden w-48 h-48 rounded-full bg-purple-900">
+                <img
+                  src={FeatureImg1}
+                  alt="feature 1"
+                  className=" object-contain"
+                ></img>
+              </div>
+              <p className="font-bold text-xl my-2 pt-2">Feature 1</p>
+              <p className="text-sm">
+                Felis consequat morbi quis turpis aenean. Velit blandit feugiat
+                in viverra ut pellentesque pretium neque. Eget habitant
+                consectetur eget non scelerisque.
+              </p>
+            </div>
+
+            <div className=" max-w-xs flex flex-col bg-yellow-400 p-8 rounded-3xl justify-center items-center text-purple-900 text-center hover:-translate-y-2 transition-transform">
+              <div className=" overflow-hidden w-48 h-48 rounded-full bg-orange-500">
+                <img
+                  src={FeatureImg2}
+                  alt="feature 2"
+                  className="object-cover object-top h-96"
+                ></img>
+              </div>
+              <p className="font-bold text-xl my-2 pt-2">Feature 2</p>
+              <p className="text-sm">
+                Felis consequat morbi quis turpis aenean. Velit blandit feugiat
+                in viverra ut pellentesque pretium neque. Eget habitant
+                consectetur eget non scelerisque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import HeroImg from "../assets/HeroImg.png";
+
+function Hero() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-8 lg:flex-row py-20">
+        {/* Left Part */}
+        <div className="flex flex-col lg:w-1/2 justify-center gap-4">
+          <h1 className="text-5xl font-black text-purple-900">
+            Welcome to OiC
+          </h1>
+          <p className="text-gray-600 text-lg ">
+            This app is still under construction.
+          </p>
+        </div>
+        {/* Right Part */}
+        <div className="lg:w-1/2">
+          <img
+            src={HeroImg}
+            alt="Hero Image"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
